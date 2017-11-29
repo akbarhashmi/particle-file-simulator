278e9d7e65b55b2851468e7c0de052f456a53a6b
Akbar Hashmi <akbar6127@gmail.com>
2017-11-29 16:40:20 +0300

    Initial Commit

---
+++ particle-fire-simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  particle-fire-simulator
+//
+//  Created by Akbar Hashmi on 11/29/17.
+//  Copyright © 2017 Akbar Hashmi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
