a0c674d3452b06fe06e391ae7dcc0baba6392d29
Akbar Hashmi <akbar6127@gmail.com>
2017-12-10 23:18:41 +0530

    Added Particle class

---
+++ particle-fire-simulator/Particle.cpp
@@ -0,0 +1,18 @@
+
+#include "Particle.hpp"
+#include <stdlib.h>
+
+namespace caveofprogramming {
+    
+    Particle::Particle() {
+        m_x = ((2.0 * rand())/RAND_MAX) - 1;
+        m_y = ((2.0 * rand())/RAND_MAX) - 1;
+    }
+    
+    Particle::~Particle() {
+        // TODO Auto-generated destructor stub
+    }
+    
+    
+    
+} /* namespace caveofprogramming */
