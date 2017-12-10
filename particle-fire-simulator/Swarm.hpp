4c1c8f3338290c70a8426e6c5e4b09ef4b385920
Akbar Hashmi <akbar6127@gmail.com>
2017-12-10 23:20:29 +0530

    Added Swarm class

---
+++ particle-fire-simulator/Swarm.hpp
@@ -0,0 +1,14 @@
+//
+//  Swarm.hpp
+//  particle-fire-simulator
+//
+//  Created by Akbar Hashmi on 12/10/17.
+//  Copyright © 2017 Akbar Hashmi. All rights reserved.
+//
+
+#ifndef Swarm_hpp
+#define Swarm_hpp
+
+#include <stdio.h>
+
+#endif /* Swarm_hpp */
