4f0dfc157dd38abb48b75a09727116d834b31f69
Akbar Hashmi <akbar6127@gmail.com>
2017-12-13 23:36:49 +0530

    Updated particle header file

--- particle-fire-simulator/Particle.hpp
+++ particle-fire-simulator/Particle.hpp
@@ -9,9 +9,14 @@ namespace caveofprogramming {
         double m_x;
         double m_y;
         
+        double m_xspeed;
+        double m_yspeed;
+        
     public:
         Particle();
         virtual ~Particle();
+        void update();
+
     };
     
 } /* namespace caveofprogramming */
